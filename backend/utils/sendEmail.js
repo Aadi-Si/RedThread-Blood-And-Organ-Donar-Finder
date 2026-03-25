f6dfe857962cbc012d236900bcae069832f64ce2
Aadi Si <aadisi1907@gmail.com>
2026-03-25 23:09:31 +0530

    mail checking

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,6 +3,7 @@ const { Resend } = require("resend");
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendEmail(to, subject, html) {
+  console.log("📧 sendEmail called with:", to);
   try {
     await resend.emails.send({
       from: "RedThread <onboarding@resend.dev>", // default working sender
@@ -10,7 +11,7 @@ async function sendEmail(to, subject, html) {
       subject,
       html,
     });
-
+    console.log("✅ Email sent");
     return true;
   } catch (err) {
     console.error("Email failed:", err);
