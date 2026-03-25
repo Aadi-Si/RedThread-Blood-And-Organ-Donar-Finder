4d6e84cde78b82b1ae09e74e84f4cc09f7bb390f
Aadi Si <aadisi1907@gmail.com>
2026-03-25 20:50:26 +0530

    Fixed app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { useEffect } from 'react'
 import Landing from './pages/Landing'
 import Login from './pages/Login'
 import Register from './pages/Register'
@@ -9,17 +8,7 @@ import ProtectedRoute from './components/ProtectedRoute'
 import AuthCallback from './pages/AuthCallback'
 import CompleteProfile from './pages/CompleteProfile'
 
-const API = import.meta.env.VITE_API_URL;
-
 const App = () => {
-
-  useEffect(() => {
-    const ping = () => fetch(`${API}/`);
-    ping();
-    const interval = setInterval(ping, 10 * 60 * 1000);
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <BrowserRouter>
       <Routes>
@@ -50,45 +39,3 @@ const App = () => {
 }
 
 export default App
-
-// import { BrowserRouter, Routes, Route } from 'react-router-dom'
-// import Landing from './pages/Landing'
-// import Login from './pages/Login'
-// import Register from './pages/Register'
-// import DonorDashboard from './pages/DonorDashboard'
-// import HospitalDashboard from './pages/HospitalDashboard'
-// import ProtectedRoute from './components/ProtectedRoute'
-// import AuthCallback from './pages/AuthCallback'
-// import CompleteProfile from './pages/CompleteProfile'
-
-// const App = () => {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<Landing />} />
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/register" element={<Register />} />
-//         <Route
-//           path="/donor/dashboard"
-//           element={
-//             <ProtectedRoute allowedRole="donor">
-//               <DonorDashboard />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route
-//           path="/hospital/dashboard"
-//           element={
-//             <ProtectedRoute allowedRole="hospital">
-//               <HospitalDashboard />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route path="/auth/callback" element={<AuthCallback />} />
-//         <Route path="/complete-profile" element={<CompleteProfile />} />
-//       </Routes>
-//     </BrowserRouter>
-//   )
-// }
-
-// export default App
