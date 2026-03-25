0b27f69779620613ff14cfadf25257bb9f3e52ba
Aadi Si <aadisi1907@gmail.com>
2026-03-25 13:46:25 +0530

    Allowed CORS

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,13 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 
+const allowedOrigins = [
+    "https://redthread-blood-and-organ-donar-finder.onrender.com",
+    "http://localhost:5173"
+];
+
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: allowedOrigins,credentials:true
 }))
 
 app.use(express.json());
