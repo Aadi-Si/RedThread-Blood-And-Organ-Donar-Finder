3772d33e553299eef31828a549266ba03ca3499b
Aadi Si <aadisi1907@gmail.com>
2026-03-31 19:20:04 +0530

    Fixed manual registering

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+const API = import.meta.env.VITE_API_URL;
 
 const Register = () => {
   const navigate = useNavigate();
